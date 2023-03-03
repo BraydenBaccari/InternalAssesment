45ba4cdcd3396418bad4ebd6337445ba725e6882
bwayden <106556922+BraydenBaccari@users.noreply.github.com>
2023-03-03 10:49:34 -0600

    Add files via upload

---
+++ InternalAsessment/src/internalasessment/InternalAsessment.java
@@ -0,0 +1,16 @@
+package internalasessment;
+import menus.StartMenu;
+/**
+ *
+ * @author b.baccari
+ */
+public class InternalAsessment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       StartMenu scrn = new StartMenu(); 
+    }
+    
+}
---
+++ InternalAsessment/src/tools/Board.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author b.baccari
+ */
+public class Board {
+
+  
+}
